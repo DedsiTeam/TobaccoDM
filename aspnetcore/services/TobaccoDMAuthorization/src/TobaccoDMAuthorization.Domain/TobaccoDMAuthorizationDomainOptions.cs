d67b4e9a893e759a67c552b0e7a69098d11afec0
Cohen <1768065921@qq.com>
2025-01-17 15:10:45 +0800

    创建用户/角色验证器。

--- aspnetcore/services/TobaccoDMAuthorization/src/TobaccoDMAuthorization.Domain/TobaccoDMAuthorizationDomainOptions.cs
+++ aspnetcore/services/TobaccoDMAuthorization/src/TobaccoDMAuthorization.Domain/TobaccoDMAuthorizationDomainOptions.cs
@@ -5,12 +5,8 @@ namespace TobaccoDMAuthorization;
 public class TobaccoDMAuthorizationDomainOptions : DedsiCleanArchitectureDomainOptions
 {
     public const string ApplicationName = "TobaccoDMAuthorization";
-    
-    public const string MobileApplicationName = "TobaccoDMAuthorization.Mobile";
-    
+
     public const string ConnectionStringName = "TobaccoDMAuthorizationDB";
     
     public const string DbSchemaName = "dbo";
-
-    public const string DbTablePrefix = "TobaccoDMAuthorization";
 }
