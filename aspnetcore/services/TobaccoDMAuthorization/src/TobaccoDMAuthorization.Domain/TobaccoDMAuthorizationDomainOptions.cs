dff2b98864c4684f007ef71cac7649acff64126d
Cohen <1768065921@qq.com>
2025-01-17 14:14:07 +0800

    重命名文件夹
    重命名文件夹

---
+++ aspnetcore/services/TobaccoDMAuthorization/src/TobaccoDMAuthorization.Domain/TobaccoDMAuthorizationDomainOptions.cs
@@ -0,0 +1,16 @@
+﻿using Dedsi.CleanArchitecture.Domain;
+
+namespace TobaccoDMAuthorization;
+
+public class TobaccoDMAuthorizationDomainOptions : DedsiCleanArchitectureDomainOptions
+{
+    public const string ApplicationName = "TobaccoDMAuthorization";
+    
+    public const string MobileApplicationName = "TobaccoDMAuthorization.Mobile";
+    
+    public const string ConnectionStringName = "TobaccoDMAuthorizationDB";
+    
+    public const string DbSchemaName = "dbo";
+
+    public const string DbTablePrefix = "TobaccoDMAuthorization";
+}
---
+++ aspnetcore/services/TobaccoDMSystemManagement/src/TobaccoDMSystemManagement.HttpApi/TobaccoDMSystemManagementController.cs
@@ -0,0 +1,10 @@
+﻿using Dedsi.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using TobaccoDMSystemManagement.Domain;
+
+namespace TobaccoDMSystemManagement;
+
+[ApiController]
+[Area(TobaccoDMSystemManagementConsts.ApplicationName)]
+[Route("api/TobaccoDMSystemManagement/[controller]/[action]")]
+public abstract class TobaccoDMSystemManagementController: DedsiControllerBase;
