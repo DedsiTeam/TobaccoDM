030b6a9de61936b6230729ca0d9edf2737ebe486
Cohen <1768065921@qq.com>
2025-01-17 19:54:01 +0800

    add java
    使用java 完成投入验收项目

---
+++ java/TobaccoDM/src/main/java/com/dedsi/tobaccodmacceptanceinput/TobaccoDmAcceptanceInputApplication.java
@@ -0,0 +1,13 @@
+package com.dedsi.tobaccodmacceptanceinput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobaccoDmAcceptanceInputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobaccoDmAcceptanceInputApplication.class, args);
+    }
+
+}
---
+++ java/TobaccoDM/src/test/java/com/dedsi/tobaccodmacceptanceinput/TobaccoDmAcceptanceInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dedsi.tobaccodmacceptanceinput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobaccoDmAcceptanceInputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
